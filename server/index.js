9d34f042b74b377f0ffddae6717c1a44db3e4204
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-11-11 15:57:46 -0800

    (feat) Add basic server, add routes for slack authentication

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-{test};
+const express = require('express');
+const { authSlackButton, authRedirect } = require('./slackAuth_handlers');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('slack-weather-bot');
+});
+
+app.get('/auth', authSlackButton);
+
+app.get('/auth/redirect', authRedirect);
+
+app.listen('3000', () => {
+  console.log('listening on 3000');
+});
