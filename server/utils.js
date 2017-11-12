09995f3c2943da24e4577cae78d3a2eaa06addfa
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-11-12 10:59:02 -0800

    (feat) Add parseSlackMessage function
    The function takes a textString that includes the @weatherbotapp
    username, and parses for the city name or help request.

--- server/utils.js
+++ server/utils.js
@@ -13,6 +13,22 @@ TempEventLog.prototype.addEvent = function (eventID) {
   return true;
 };
 
+const parseSlackMessage = (botUsername, text) => {
+  return new Promise((resolve) => {
+    if (text.includes(`${botUsername} help`)) {
+      resolve('help');
+    }
+    const start = text.indexOf('weather in') + 11;
+    const finish = text.indexOf('?');
+    if (start <= 11 || finish <= start) {
+      resolve('queryError');
+    }
+    const city = text.slice(start, finish).split(' ').join('');
+    resolve(city);
+  });
+};
+
 module.exports = {
   TempEventLog,
+  parseSlackMessage,
 };
