b2d05546e331762f42578b6992e042f9566af9c0
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-11-12 10:57:39 -0800

    (feat) add botDialogue object, contains various bot responses

---
+++ server/botDialogue.js
@@ -0,0 +1,7 @@
+module.exports = {
+  botHelp: 'I\'m a simple weather bot. Enter \'@weatherappbot What\'s the weather in CITY_NAME?\' to learn the current weather in that city.\n For example: \n\'@weatherappbot What\'s the weather in San Francisco?\'\n For a more specific search, use \'CITY_NAME, TWO_LETTER_COUNTRY_CODE.\' \n For example: \n\'@weatherappbot What\'s the weather in Paris, US?\', to recieve the weather in Paris, Texas instead of Paris, France. \n If you don\'t know your two letter country code, visit this link: https://en.wikipedia.org/wiki/ISO_3166-2',
+  botMentioned: 'Spinning my gears...',
+  cityNotFoundError: 'I\'m sorry, I couldn\'t find that city.',
+  weatherAPIError: 'I\'m sorry, I\'m experiencing technical difficulties fetching the weather.',
+  querySyntaxError: 'Whoops, there\'s a problem with your request syntax.',
+};
